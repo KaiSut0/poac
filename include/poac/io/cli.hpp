2b1dcc3d95c877d3c5602ae50f57b2697d687322
matken11235 <matken11235@gmail.com>
2018-08-17 17:13:00 +0900

    Add workflows

--- include/poac/io/cli.hpp
+++ include/poac/io/cli.hpp
@@ -4,26 +4,25 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <string_view>
 #include <cstdio>
 #include <iomanip>
 
 
 namespace poac::io::cli {
     // Clear screen
-    static constexpr std::string_view clr_screen = "\x1b[2J";
+    static const std::string clr_screen = "\x1b[2J";
     // Clear from the cursor position to the right end
-    static constexpr std::string_view clr_right = "\x1b[0K";
+    static const std::string clr_right = "\x1b[0K";
     // Clear from the cursor position to the left end
-    static constexpr std::string_view clr_left = "\x1b[1K";
+    static const std::string clr_left = "\x1b[1K";
     // Clear the line at the cursor position
-    static constexpr std::string_view clr_line = "\x1b[2K";
+    static const std::string clr_line = "\x1b[2K";
     // Move cursor position
-    const std::string_view up(unsigned int y)    { return "\x1b["+std::to_string(y)+"A"; }
-    const std::string_view down(unsigned int y)  { return "\x1b["+std::to_string(y)+"B"; }
-    const std::string_view right(unsigned int x) { return "\x1b["+std::to_string(x)+"C"; }
-    const std::string_view left(unsigned int x)  { return "\x1b["+std::to_string(x)+"D"; }
-    const std::string_view location(unsigned int x, unsigned int y) {
+    const std::string up(unsigned int y)    { return "\x1b["+std::to_string(y)+"A"; }
+    const std::string down(unsigned int y)  { return "\x1b["+std::to_string(y)+"B"; }
+    const std::string right(unsigned int x) { return "\x1b["+std::to_string(x)+"C"; }
+    const std::string left(unsigned int x)  { return "\x1b["+std::to_string(x)+"D"; }
+    const std::string location(unsigned int x, unsigned int y) {
         return "\x1b["+std::to_string(x)+";"+std::to_string(y)+"H";
     }
 
@@ -36,13 +35,13 @@ namespace poac::io::cli {
 
     void set_left(const int&& n) { std::cout << std::setw(n) << std::left; }
 
-    static constexpr std::string_view red = "\x1b[31m";
-    static constexpr std::string_view green = "\x1b[32m";
-    static constexpr std::string_view yellow = "\x1b[33m";
-    static constexpr std::string_view blue = "\x1b[34m";
-    static constexpr std::string_view bold = "\x1b[1m";
-    static constexpr std::string_view underline = "\x1b[4m";
-    static constexpr std::string_view reset = "\x1b[0m";
+    static const std::string red = "\x1b[31m";
+    static const std::string green = "\x1b[32m";
+    static const std::string yellow = "\x1b[33m";
+    static const std::string blue = "\x1b[34m";
+    static const std::string bold = "\x1b[1m";
+    static const std::string underline = "\x1b[4m";
+    static const std::string reset = "\x1b[0m";
 
     const std::vector<std::string> spinners{ "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" };
 } // end namespace
